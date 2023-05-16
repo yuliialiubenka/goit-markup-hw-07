b88bed957bd82eb334dc8680048916355e6b5378
Yuliia Liubenka <yuliialiubenka@gmail.com>
2023-05-17 01:04:56 +0300

    Hometask #7

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  };
+
+  function hideBody() {
+    refs.modal.classList.add("overflow-hidden");
+  };
+  function showBody() {
+    refs.modal.classList.remove("overflow-hidden");
+  };
+})();
